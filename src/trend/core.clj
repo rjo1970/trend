4760700ab84a11c84d512df88a54110b1e049ec7
R.J. Osborne <rosborne@pillartechnology.com>
2015-11-13 16:20:29 -0500

    Added quickie autotest plugin, refactored let binding to thread-last construct, removed unneeded if-let

--- src/trend/core.clj
+++ src/trend/core.clj
@@ -1,17 +1,18 @@
 (ns trend.core)
 
-(defn trend-pairs [s]
-  (if-let [pairs (partition 2 1 s)]
-    pairs nil))
+(defn- trend-pairs [s]
+  (partition 2 1 s))
 
-(defn pair-score [[p1 p2]]
+(defn- pair-score [[p1 p2]]
   (cond
     (and (zero? p1) (> p2 0)) 1
     (= p1 p2) 0
     :else (-> (- p2 p1) (/ p1))))
 
+(defn- nil-or-empty? [s]
+  (or (nil? s) (empty? s)))
+
 (defn trend [s]
-  (let [nil-or-empty? (or (nil? s) (empty? s))
-        pairs (trend-pairs s)
-        scores (map pair-score pairs)]
-    (if nil-or-empty? nil (reduce + scores))))
+    (if (nil-or-empty? s)
+      nil
+      (->> (trend-pairs s) (map pair-score) (reduce +))))
